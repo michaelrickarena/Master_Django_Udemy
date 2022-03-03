a47fcf47b6cacba9803a42e7d043026b3e7d7510
Michael Arena <michaelrickarena@gmail.com>
2022-03-03 00:32:32 -0500

    up to section 4 complete

--- hofvidz-project/halls/models.py
+++ hofvidz-project/halls/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Hall(models.Model):
 	title = models.CharField(max_length=255)
+	#connect to a user
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Video(models.Model):
--- hofvidz-project/halls/views.py
+++ hofvidz-project/halls/views.py
@@ -1,13 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from .models import Hall
+
+
 
 def home(request):
 	return render(request,'halls/home.html')
-# Create your views here.
+
+def dashboard(request):
+	return render(request,'halls/dashboard.html')
+
 
 class SignUp(generic.CreateView):
 	form_class = UserCreationForm
 	success_url = reverse_lazy('home')
 	template_name = 'registration/signup.html' # must have this name unless changed
+
+	def form_valid(self,form):
+		view = super(SignUp, self).form_valid(form)
+		username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+		user = authenticate(username=username, password=password)
+		login(self.request, user)
+		return view
+
+class CreateHall(generic.CreateView):
+	model = Hall
+	fields = ['title']
+	template_name = 'halls/create_hall.html'
+	success_url = reverse_lazy('dashboard')
+
+
+	# check if user is valid
+
+	def form_valid(self,form):
+		form.instance.user = self.request.user
+		super(CreateHall, self).form_valid(form)
+
+		return redirect('home')
+
+class DetailHall(generic.DetailView):
+	model = Hall
+	template_name = 'halls/detail_hall.html'
+
+class UpdateHall(generic.UpdateView):
+	model = Hall
+	template_name = 'halls/update_hall.html'
+	fields = ['title']
+	success_url = reverse_lazy('dashboard')
+
+class DeleteHall(generic.DeleteView):
+	model = Hall
+	template_name = 'halls/delete_hall.html'
+	fields = ['title']
+	success_url = reverse_lazy('dashboard')
+
